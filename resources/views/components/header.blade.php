400d5ab8681c715752a97b8c284e7505cd1950ec
abdirahmanWeheliye <159246070+abdirahmanWeheliye@users.noreply.github.com>
2024-11-22 11:21:51 +0100

    kleine verandering aan de home.blade , linkjes weg gehaald

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,15 +1,16 @@
 <header class="flex items-center justify-between p-2.5 bg-gray-600 text-white">
     <div class="logo" style="flex: 1;">
-    <a href="/" style="text-decoration: none; font-size: 24px; font-weight: bold; color: ">Voetbal</a>
+        <a href="/" style="text-decoration: none; font-size: 24px; font-weight: bold; color: ">Voetbal</a>
     </div>
     <nav style="flex: 2;">
         <ul style="display: flex; justify-content: space-around; list-style: none; padding: 0; margin: 0;">
             <li><a style="text-decoration: none;" href="{{ route('teams.create') }}">Team maken</a></li>
             <li><a href="/" style="text-decoration: none;">Scoreboard</a></li>
             <li><a href="/" style="text-decoration: none; ">Sport</a></li>
-            
+
             @guest
-                <li><a href="{{ auth()->check() ? route('advertenties.create') : route('login') }}" style="text-decoration: none; color: ">inloggen</a></li>
+                <li><a href="{{ auth()->check() ? route('advertenties.create') : route('login') }}"
+                        style="text-decoration: none; color: ">inloggen</a></li>
                 <li><a href="{{ route('register') }}" style="text-decoration: none; ">Registreren</a></li>
             @endguest
 
