74cfb92a413401664acbfdf0689d76d9a90fa65a
abdirahmanWeheliye <159246070+abdirahmanWeheliye@users.noreply.github.com>
2024-12-16 09:08:15 +0100

    css voor scoreboard pagina, tournament pagina  en admin panel

--- resources/views/scoreboard/index.blade.php
+++ resources/views/scoreboard/index.blade.php
@@ -4,28 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Document</title>
 </head>
 <body>
-    <h1>Scoreboard</h1>
-    <table>
-        <th>
-            <th class="border px-4 py-2">Id</th>
-            <th class="border px-4 py-2">Team Name</th>
-            <th class="border px-4 py-2">Score</th>
-        </th>
-        <tb>
-            @foreach ($scores as $score)
-            <tr>
-                <td class="border px-4 py-2">{{ $score->id }}</td>
-                <td class="border px-4 py-2">{{ $score->name }}</td>
-                <td class="border px-4 py-2">{{ $score->score }}</td>
-            </tr>
-        </tb>
-        @endforeach
+    <header>
+<x-header/>
+    </header>
+    <h1 class="text-xl flex justify-center font-bold mb-[2.5rem]">Scoreboard</h1>
+    <div class="flex justify-center">
+        <table class="mx-[10rem] text-center">
+            <th>
+                <th class="border px-4 py-2">Id</th>
+                <th class="border px-4 py-2">Team name</th>
+                <th class="border px-4 py-2">Points</th>
+            </th>
+            <tb>
+                @foreach ($scores as $score)
+                <tr>
+                    <td class="border px-4 py-2">{{ $score->id }}</td>
+                    <td class="border px-4 py-2">{{ $score->name }}</td>
+                    <td class="border px-4 py-2">{{ $score->score }}</td>
+                </tr>
+            </tb>
+            @endforeach
 
 
-    </table>
+        </table>
+    </div>
 
+<footer class="mt-[25rem]">
+    <x-footer/>
+</footer>
 </body>
 </html>
--- resources/views/tournaments/index.blade.php
+++ resources/views/tournaments/index.blade.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Document</title>
 </head>
 <body>
+    <header>
+        <x-header/>
+    </header>
 <p>dit is de toernooi pagina</p>
+<footer  class="mt-[38rem]">
+    <x-footer/>
+</footer>
 </body>
 </html>
